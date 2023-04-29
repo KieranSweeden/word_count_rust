1f5ef4fb7d4c443b6d5150db2b5270e9d328ef65
kieran <kieransweeden@gmail.com>
2023-04-29 18:13:11 +0100

    Add prompt for user input to get path string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    intro();
+
+    loop {
+        let path = prompt_user_for_file_path();
+        println!("You've provided the path of {}", path);
+    }
+}
+
+fn prompt_user_for_file_path() -> String {
+    println!("Please provide a path to the target file:");
+
+    let mut input = String::new();
+
+    io::stdin()
+        .read_line(&mut input)
+        .expect("No input was provided");
+
+    input.trim().to_string()
+}
+
+fn intro() {
+    println!("=====================");
+    println!("Welcome to word count");
+    println!("=====================");
 }
